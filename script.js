c7544d9aea644cff0cc20915f5f4dd40c60ff6e0
pausenfm <schulze@soundinnovationlab.com>
2025-04-04 17:41:54 +0200

    update web app

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function connectWebSocket() {
   if (socket && socket.readyState === WebSocket.OPEN) return;
 
   console.log("🔄 Verbinde WebSocket...");
-  socket = new WebSocket("wss://e35f-2a02-3100-2eb9-8600-29e6-b95b-10f1-6f30.ngrok-free.app");
+  socket = new WebSocket("wss://6605-2a02-3100-2eb9-8600-29e6-b95b-10f1-6f30.ngrok-free.app");
 
   socket.onopen = () => {
     console.log("✅ WebSocket verbunden!");
